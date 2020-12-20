5335a96ee8d976b6d8b2dc26b5283ec2beeaf210
diamondgirl <diamondgirl@pop-os.localdomain>
2020-12-20 21:29:57 +0100

    add fizzbuzz

---
+++ fizzbuzz/index.js
@@ -0,0 +1,11 @@
+console.log("Hello, Aashritha!");
+
+for (var noth = 1; noth<21; noth++) {
+    if ((noth == 3) || (noth == 6)) {
+        console.log("Fizz");
+    } else {
+        console.log(noth);
+    }
+}
+
+
