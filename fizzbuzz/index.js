0ce852e61fb9ee80d972a5b1c33526ee7e361923
Diamant Girl <diamantgirl@localhost>
2020-12-21 17:00:39 +0100

    Add checks for 9, 10, 12, 15, 18, 20

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,10 +1,18 @@
 console.log("Hello, Aashritha!");
 
 for (var noth = 1; noth<21; noth++) {
-    if ((noth == 3) || (noth == 6)) {
+    if ((noth == 3) || (noth == 6) || (noth == 9) || (noth == 12) || (noth == 18) ) {
         console.log("Fizz");
     } else {
-        console.log(noth);
+        if ((noth == 5) || (noth == 10) || (noth == 20)) {
+            console.log("Buzz")
+        } else {
+            if (noth == 15) {
+                console.log("Fizz Buzz")
+            } else {
+                console.log(noth)
+            }
+        }
     }
 }
 
